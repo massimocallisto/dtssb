f13069183ec8c849828670fa1f5a1c46521c6e71
Massimo Callisto <50881651+massimocallisto@users.noreply.github.com>
2026-03-19 09:06:21 +0100

    aggiornato pull, da testare

--- pull.php
+++ pull.php
@@ -1,1 +1,1 @@
-<?php shell_exec( 'cd /var/www/html/dtssb; git reset --hard origin/2026; git clean -d -f; git pull https://github.com/massimocallisto/dtssb 2026' ); ?>
+<?php shell_exec( 'cd /var/www/html/dtssb; git reset --hard origin/main; git clean -d -f; git pull https://github.com/massimocallisto/dtssb main' ); ?>
